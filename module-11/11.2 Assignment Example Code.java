e5eb6e5be60fee8ce406ce39804d214198be9ae7
Angela Vargas <silver.dawn166@gmail.com>
2026-03-14 18:11:27 -0600

    Add module-11 with assignment files

---
+++ module-11/11.2 Assignment Example Code.java
@@ -0,0 +1,21 @@
+//Angela Vargas CSD430 11.2 Assignment
+
+// Example 1 – Poor Coding Style
+public class test{
+public static void main(String[] args){
+int a=5;int b=10;System.out.println(a+b);}
+}
+
+// Example 2 – Follows Standard Rules
+public class AdditionExample {
+
+    public static void main(String[] args) {
+
+        int firstNumber = 5;
+        int secondNumber = 10;
+
+        int sum = firstNumber + secondNumber;
+
+        System.out.println("Sum: " + sum);
+    }
+}
